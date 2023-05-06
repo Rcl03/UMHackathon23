0f8371070cf745e3145bc1dea94f93984c709b7f
Rcl03 <130187034+Rcl03@users.noreply.github.com>
2023-05-07 02:02:23 +0800

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -69,43 +69,32 @@ def run_website():
             # Display the graph in Streamlit
             st.plotly_chart(fig)
         else:
-                variables = [
-                    'Information Technology', 'Software', 'Mobile Apps', 'Internet',
-                    'Artificial Intelligence (AI)', 'Internet of Things (IoT)', 'Web Development',
-                    'Cloud', 'Automation', 'Big Data', 'Machine Learning', 'Robotics',
-                    'Blockchain', 'Augmented Reality (AR)', 'Virtual Reality (VR)', 'Smart Home',
-                    'Clean Energy', 'Sensor', 'Nanotechnology', 'Developer APIs'
-                ]
+            industry_categories = ['Technology', 'Finance', 'Health and Wellness', 'Retail and E-commerce', 'Education',
+                                   'Media and Entertainment', 'Travel and Hospitality', 'Marketing and Advertising',
+                                   'Human Resources', 'Real Estate and Property', 'Food and Beverage', 'Others']
 
-                categories = [
-                    'category_0', 'category_1', 'category_2', 'category_3',
-                    'category_4', 'category_5', 'category_6', 'category_7',
-                    'category_8'
-                ]
+            # Calculate the average revenue growth rate for each industry category
+            average_revenue_growth = {}
 
-                average_mean_revenues = []
+            for category in industry_categories:
+                category_df = data[data[category] == 1]
+                average_growth = category_df['revenue_growth(%)'].mean()
+                average_revenue_growth[category] = average_growth
 
-                # Loop through each variable
-                for variable in variables:
-                    # Select rows where the variable is present in any of the category columns
-                    variable_companies = data[data[categories].apply(lambda x: variable in x.values, axis=1)]
+            # Create a Bar object
+            bar = go.Bar(x=list(average_revenue_growth.keys()), y=list(average_revenue_growth.values()))
 
-                    # Calculate the average mean revenue for the variable
-                    average_mean_revenue = variable_companies['revenue_c'].mean()
-                    average_mean_revenues.append((variable, average_mean_revenue))
+            # Set the layout
+            layout = go.Layout(title='Average Revenue Growth Rate by Industry Category',
+                               xaxis=dict(title='Industry Category'),
+                               yaxis=dict(title='Average Revenue Growth Rate (%)'),
+                               height=500)
 
-                # Extract the categories and average revenues for plotting
-                variable_labels = [x[0] for x in average_mean_revenues]
-                mean_revenues = [x[1] for x in average_mean_revenues]
+            # Create a Figure object
+            fig = go.Figure(data=[bar], layout=layout)
 
-                # Create a DataFrame for the data
-                data_plot = pd.DataFrame({'Variable': variable_labels, 'Mean Revenue': mean_revenues})
-
-                # Create the bar graph using Plotly
-                fig = px.bar(data_plot, x='Variable', y='Mean Revenue', title='Average Mean Revenue by Variable')
-
-                # Display the graph in Streamlit
-                st.plotly_chart(fig)
+            # Display the graph in Streamlit
+            st.plotly_chart(fig)
 
         # Create a Streamlit app
         st.title('Total funding vs Total revenue')
