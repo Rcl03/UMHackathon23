d42e444124262bd6ec8f39e9904e02c06d2a0959
Rcl03 <130187034+Rcl03@users.noreply.github.com>
2023-05-07 03:14:40 +0800

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -630,19 +630,23 @@ def run_website():
         # Display the plot
         st.plotly_chart(fig)
 
-        # Define the range of total funding to display in the bar chart
-            total_fund_min = st.sidebar.slider("Minimum Total Funding", 0, 600000000, 0, 1000000)
-            total_fund_max = st.sidebar.slider("Maximum Total Funding", 0, 600000000, 600000000, 1000000)
-
-            # Filter the data based on the selected range of total funding
-            filtered_data = filter_data(total_fund_min, total_fund_max)
-
-            # Create a button to generate the bar chart
-            if st.button("Generate Bar Chart"):
+        # Define the range of total funding to display in the bar char
+        total_fund_min = st.slider("Minimum Total Funding", 0, 600000000, 0, 1000000)
+        total_fund_max = st.slider("Maximum Total Funding", 0, 600000000, 600000000, 1000000)
+        
+        filtered_data = filter_data(total_fund_min, total_fund_max)
+        
+        if st.button("Generate Bar Chart"):
                 # Generate the bar chart
                 bar_chart = generate_bar_chart(filtered_data)
                 # Display the bar chart
-                st.altair_chart(bar_chart, use_container_width=True)
+                st.altair_chart(bar_chart, use_container_width=True)   
+
+            # Filter the data based on the selected range of total funding
+          
+
+            # Create a button to generate the bar chart
+                 
 
 
 
